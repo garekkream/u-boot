e8796583e1f4168f43723a6ed35934980460d914
Jagan Teki <jteki@openedev.com>
2016-02-14 07:45:02 +0530

    configs: Use CONFIG_SPI_NOR_BAR
    CONFIG_SPI_FLASH_BAR => CONFIG_SPI_NOR_BAR
    
    Cc: Simon Glass <sjg@chromium.org>
    Cc: Bin Meng <bmeng.cn@gmail.com>
    Cc: Mugunthan V N <mugunthanvnm@ti.com>
    Cc: Michal Simek <michal.simek@xilinx.com>
    Cc: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Jagan Teki <jteki@openedev.com>

--- include/configs/gw_ventana.h
+++ include/configs/gw_ventana.h
@@ -69,7 +69,7 @@
 #ifdef CONFIG_CMD_SF
   #define CONFIG_MXC_SPI
   #define CONFIG_SPI_FLASH_MTD
-  #define CONFIG_SPI_FLASH_BAR
+  #define CONFIG_SPI_NOR_BAR
   #define CONFIG_SF_DEFAULT_BUS              0
   #define CONFIG_SF_DEFAULT_CS               0
 					     /* GPIO 3-19 (21248) */
