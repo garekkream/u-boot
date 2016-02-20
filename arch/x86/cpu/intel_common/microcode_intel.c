a61e83f1055bf29b5e8a56cd10b23c82d1abf271
Simon Glass <sjg@chromium.org>
2016-02-20 10:25:59 -0700

    x86: Move microcode code to a common location
    This code is used on several Intel CPUs. Move it into a common location.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- arch/x86/cpu/intel_common/microcode_intel.c
+++ arch/x86/cpu/intel_common/microcode_intel.c
@@ -12,10 +12,10 @@
 #include <fdtdec.h>
 #include <libfdt.h>
 #include <asm/cpu.h>
+#include <asm/microcode.h>
 #include <asm/msr.h>
 #include <asm/msr-index.h>
 #include <asm/processor.h>
-#include <asm/arch/microcode.h>
 
 /**
  * struct microcode_update - standard microcode header from Intel
